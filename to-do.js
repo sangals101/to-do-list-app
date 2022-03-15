79519468b46d2fc3e72d0bb6a6514cf9b829d40c
Shivam Sangal <shsangal@deloitte.com>
2022-03-15 15:13:50 +0530

    to do list app

---
+++ to-do.js
@@ -0,0 +1,34 @@
+function displayTodo() {
+  let todo = localStorage.getItem("todo");
+  if (todo === null) {
+    todoArray = [];
+  } else {
+    todoArray = JSON.parse(todo);
+  }
+  let htmlCode = "";
+  todoArray.forEach((list, ind) => {
+    htmlCode += `<div class='flex mb-4 items-center'>
+    <p class='w-full text-grey-darkest'>${list}</p>
+    <button onclick='edit(${ind})' class='flex-no-shrink p-2 ml-4 mr-2 border-2 rounded text-white text-grey bg-green-600'>Edit</button>
+    <button onclick='deleteTodo(${ind})' class='flex-no-shrink p-2 ml-2 border-2 rounded text-white bg-red-500'>Delete</button>
+ </div>`;
+  });
+  listBox.innerHTML = htmlCode;
+}
+
+function deleteTodo(ind) {
+  let todo = localStorage.getItem("todo");
+  todoArray = JSON.parse(todo);
+  todoArray.splice(ind, 1);
+  localStorage.setItem("todo", JSON.stringify(todoArray));
+  displayTodo();
+}
+
+function edit(ind) {
+  saveInd.value = ind;
+  let todo = localStorage.getItem("todo");
+  todoArray = JSON.parse(todo);
+  text.value = todoArray[ind];
+  addTaskButton.style.display = "none";
+  saveTaskButton.style.display = "block";
+}
